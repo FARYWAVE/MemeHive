e2559e59379825b915c8f070bcfc5988fe05f860
FARYWAVE <farywave.official@gmail.com>
2026-05-10 15:49:28 +0300

    New app logo

--- app/src/main/java/com/farywave/memehive/core/DeviceTools.kt
+++ app/src/main/java/com/farywave/memehive/core/DeviceTools.kt
@@ -120,7 +120,7 @@ object DeviceTools {
                 context,
                 MemeHiveApplication.PICKER_CHANNEL_ID
             )
-                .setSmallIcon(R.drawable.ic_app_logo)
+                .setSmallIcon(R.drawable.ic_launcher_monochrome)
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(context.getString(R.string.picket_notification_text))
                 .setContentIntent(pendingIntent)
